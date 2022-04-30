66106c66e615d3dec23264ae400554fa94dbbdb4
dkwitt <56313807+dkwitt@users.noreply.github.com>
2022-04-30 10:30:24 +0200

    Add files via upload

---
+++ Cw05/zadanie3/README.md
@@ -0,0 +1,3 @@
+**Zadanie 3**
+
+![](https://gitlab.com/opengl-2021/dominika-witt/-/raw/main/Cw05/zadanie3/zadanie3.gif)
---
+++ Cw05/zadanie3/vertex.glsl
@@ -0,0 +1,22 @@
+#version 330
+
+// Wspolrzedne wierzcholkow
+layout( location = 0 ) in vec4 inPosition;
+layout( location = 1 ) in vec3 inNormal;
+layout( location = 2 ) in vec2 inUV;
+// Macierz rzutowania
+uniform mat4 matProjView;
+uniform mat4 matModel;
+
+out vec4 inoutPos;
+out vec3 inoutNormal;
+out vec2 wspolrzedneUV;
+
+void main()
+{
+	wspolrzedneUV = inUV;
+	gl_Position = matProjView * matModel * inPosition;
+	inoutPos = inPosition;
+	inoutNormal = inNormal;
+	wspolrzedneUV = inUV;
+}
